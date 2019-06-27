af436574b4f964ba89cac49a7da576bda5804b7d
jj.jorgecastillo <jj.jorgecastillo@gmail.com>
2019-06-26 23:34:13 -0500

    subida2

--- src/index.js
+++ src/index.js
@@ -5,13 +5,3 @@ import App from './App';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-/** $ git pull
-There is no tracking information for the current branch.
-Please specify which branch you want to merge with.
-See git-pull(1) for details.
-
-    git pull <remote> <branch>
-
-If you wish to set tracking information for this branch you can do so with:
-
-    git branch --set-upstream-to=origin/<branch> master */
